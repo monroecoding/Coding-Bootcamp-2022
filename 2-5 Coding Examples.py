f06511180cfa756e5c1f1cc521c6f2cc4f7696fd
monroecoding <94639671+monroecoding@users.noreply.github.com>
2021-11-29 14:34:53 -0500

    Add files via upload

---
+++ 2-5 Coding Examples.py
@@ -0,0 +1,43 @@
+"""
+Coding Bootcamp at Monroe Township Library
+
+2/5 Coding Examples
+"""
+
+
+#print function
+print("Hello World!")
+print(17)
+
+
+#primitive data types
+string = "This is a string"
+integer = 23
+floating_point = 3.14
+boolean = True
+none_type = None
+
+    
+print(
+      type(string),
+      type(integer),
+      type(floating_point),
+      type(boolean),
+      type(none_type)
+      )
+
+
+#naming variables
+name = "John" #valid variable name
+ename = "John" #variables cannot start with a number, *comment this out before running*
+multi_word_variable = True #separate multi-word variables with underscores
+
+
+#This is a comment! The computer will ignore this when the program is run
+#num = 12
+#print(num)
+
+
+#inputs
+first_name = input("Enter your first name: ") #asks the user for input and saves it to variable
+print(first_name)
